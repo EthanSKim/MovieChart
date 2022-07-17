5f1bc1b687002e76d87170f4e70c640ebd59ebf5
Ethan Kim <ethankim9091@gmail.com>
2022-07-17 02:15:52 -0500

    Added navigation component

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Navigation from './components/Navigation';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Navigation />
     <App />
   </React.StrictMode>
 );
